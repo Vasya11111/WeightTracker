1eae2f6c9a8d4ffebffe9bc210bd8d5bc7772748
VASYA <vas.chuev@gmail.com>
2023-04-08 13:05:01 +0300

    Во фрагменте со списком измерений кнопка добавления перенесена в центр и исправлено отображение RecyclerView, также график GraphView заменен на MPAndroidChart

--- app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.kt
@@ -4,29 +4,32 @@ package com.example.myapplication.ui.notifications
 
 
 
-import android.content.ContentValues
+
 import android.content.Context
-import android.database.sqlite.SQLiteDatabase
-import android.database.sqlite.SQLiteOpenHelper
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.Database
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentNotificationsBinding
-import com.jjoe64.graphview.GraphView
-import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.LimitLine
+import com.github.mikephil.charting.components.LimitLine.LimitLabelPosition
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
 import java.text.SimpleDateFormat
 import java.util.*
-
-
 
 
 class NotificationsFragment : Fragment() {
@@ -86,7 +89,7 @@ for(i in allpoints){
 
         super.onResume()
 
-        val graph = getView()?.findViewById<GraphView >(R.id.graph)// as GraphView
+        """val graph = getView()?.findViewById<GraphView >(R.id.graph)// as GraphView
 
 
 
@@ -101,7 +104,63 @@ for(i in allpoints){
 
         graph?.getViewport()?.setYAxisBoundsManual(true);
         graph?.getViewport()?.setMinY(0.0);
-        graph?.getViewport()?.setMaxY(200.0);
+        graph?.getViewport()?.setMaxY(200.0);"""
+
+        val chart = requireView().findViewById(R.id.chart) as LineChart
+
+        val entries: ArrayList<Entry> = ArrayList<Entry>()
+
+        val xAxis: XAxis = chart.getXAxis()
+        val formatter: ValueFormatter = object : ValueFormatter() {
+            private val mFormat = SimpleDateFormat("dd MMM", Locale.ENGLISH)
+
+            override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+                return mFormat.format(Date(value.toLong()))
+            }
+        }
+
+        xAxis.valueFormatter = formatter
+
+        val dateFormat = SimpleDateFormat("dd MMM", Locale.ENGLISH)
+
+        for(i in allpoints){
+
+            entries.add(Entry(i.date.time.toFloat(), i.weight.toFloat()))
+        }
+
+
+       // entries.add(Entry(dateFormat.parse("01 Jul").time.toFloat(), 100f))
+       // entries.add(Entry(dateFormat.parse("02 Jul").time.toFloat(), 120f))
+        //entries.add(Entry(dateFormat.parse("03 Jul").time.toFloat(), 130f))
+        val dataSet = LineDataSet(entries, "Вес КГ")
+        val lineData = LineData(dataSet)
+        chart.data = lineData
+
+
+        val ll1 = LimitLine(80f, "Upper Limit")
+        ll1.lineWidth = 4f
+        ll1.enableDashedLine(10f, 10f, 0f)
+        ll1.labelPosition = LimitLabelPosition.RIGHT_TOP
+        ll1.textSize = 10f
+        ll1.lineColor=R.color.red
+
+
+
+        val ll2 = LimitLine(85f, "Upper Limit")
+        ll2.lineWidth = 4f
+        ll2.enableDashedLine(10f, 10f, 0f)
+        ll2.labelPosition = LimitLabelPosition.RIGHT_TOP
+        ll2.textSize = 10f
+
+
+        val yAxis: YAxis = chart.getAxis(YAxis.AxisDependency.LEFT)
+        yAxis.addLimitLine(ll1)
+        yAxis.addLimitLine(ll2)
+
+
+
+
+        chart.invalidate() // refresh
 
 
     }
